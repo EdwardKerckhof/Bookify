20a90d1fc942b386bb2810fb799e1bbfed1e5a8d
EdwardKerckhof <edwardkerckhof@hotmail.com>
2024-05-31 18:22:59 +0200

    [feat]: booking entity

---
+++ Bookify.Domain/Bookings/DateRange.cs
@@ -0,0 +1,22 @@
+﻿namespace Bookify.Domain.Bookings;
+
+public record DateRange
+{
+    private DateRange() { }
+
+    public DateOnly Start { get; private init; }
+
+    public DateOnly End { get; private init; }
+
+    public int LengthInDays => End.DayNumber - Start.DayNumber;
+
+    public static DateRange Create(DateOnly start, DateOnly end)
+    {
+        if (start > end)
+        {
+            throw new ArgumentException("Start date must be before end date.");
+        }
+
+        return new DateRange { Start = start, End = end };
+    }
+}
