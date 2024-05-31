777592d45d5883719d1fdd7d41dff09b548c0548
EdwardKerckhof <edwardkerckhof@hotmail.com>
2024-05-31 22:12:19 +0200

    [feat]: command validation pipeline

--- Bookify.Application/DependencyInjection.cs
+++ Bookify.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using Bookify.Application.Abstractions.Behaviors;
 using Bookify.Domain.Bookings;
 
+using FluentValidation;
+
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bookify.Application;
@@ -14,7 +16,11 @@ public static class DependencyInjection
             configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
 
             configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
+
+            configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
+        
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
 
         services.AddTransient<PricingService>();
 
---
+++ Bookify.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookify.Application.Exceptions;
+
+public class ValidationException : Exception
+{
+    public IEnumerable<ValidationError> Errors { get; }
+
+    public ValidationException(List<ValidationError> errors)
+    {
+        Errors = errors;
+    }
+}
