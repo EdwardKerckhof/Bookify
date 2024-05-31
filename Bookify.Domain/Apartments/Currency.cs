af19ec3d712dead095a359f24b6cebf15285dfe6
EdwardKerckhof <edwardkerckhof@hotmail.com>
2024-05-31 17:32:15 +0200

    [init]: setup base project

---
+++ Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,24 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Currency
+{
+    internal static readonly Currency None = new("");
+    public static readonly Currency Usd = new("USD");
+    public static readonly Currency Eur = new("EUR");
+
+    public static readonly IReadOnlyCollection<Currency> All = new[]
+    {
+        Usd,
+        Eur
+    };
+
+    public string Code { get; init; }
+
+    private Currency(string code) => Code = code;
+
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(x => x.Code == code) ??
+               throw new ApplicationException($"Invalid currency code: {code}");
+    }
+}
---
+++ Bookify.Domain/Apartments/Money.cs
@@ -0,0 +1,16 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Money(decimal Amount, Currency Currency)
+{
+    public static Money operator +(Money first, Money second)
+    {
+        if (first.Currency != second.Currency)
+        {
+            throw new InvalidOperationException("Cannot add money with different currencies");
+        }
+
+        return first with { Amount = first.Amount + second.Amount };
+    }
+
+    public static Money Zero() => new(0, Currency.None);
+}
