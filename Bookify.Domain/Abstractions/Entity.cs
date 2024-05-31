5c4ff4298b91efc8244b6b156f1323b51311c2bb
EdwardKerckhof <edwardkerckhof@hotmail.com>
2024-05-31 18:00:29 +0200

    [feat]: introducing domain events

--- Bookify.Domain/Abstractions/Entity.cs
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -2,10 +2,17 @@
 
 public abstract class Entity
 {
+    private readonly List<IDomainEvent> _domainEvents = new();
     public Guid Id { get; init; }
 
     protected Entity(Guid id)
     {
         Id = id;
     }
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents() => _domainEvents.ToList();
+    
+    public void ClearDomainEvents() => _domainEvents.Clear();
+    
+    protected void RaiseDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
 }
