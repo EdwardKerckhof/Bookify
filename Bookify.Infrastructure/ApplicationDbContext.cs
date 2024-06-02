4a26ecfa9b54eb0c2e8716d11bfa3418b04b09bd
EdwardKerckhof <edwardkerckhof@hotmail.com>
2024-06-02 15:54:07 +0200

    [feat]: automatically applying entity configurations

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -7,4 +7,11 @@ namespace Bookify.Infrastructure;
 public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
     public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
