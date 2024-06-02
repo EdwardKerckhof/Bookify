e0cc93bcbe62266abd588169bf9abd1adb419fed
EdwardKerckhof <edwardkerckhof@hotmail.com>
2024-06-02 15:39:02 +0200

    [feat]: adding EF Core

---
+++ Bookify.Infrastructure/ApplicationDbContact.cs
@@ -0,0 +1,10 @@
+﻿using Bookify.Domain.Abstractions;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure;
+
+public sealed class ApplicationDbContact : DbContext, IUnitOfWork
+{
+    public ApplicationDbContact(DbContextOptions options) : base(options) { }
+}
